1d73dc1386070e0971b90a9c2902536a683de212
frendyardiansyah <frendy.ardiansyah@uiii.ac.id>
2025-05-01 22:47:00 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+export default defineConfig([
+  daStyle,
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+]);
